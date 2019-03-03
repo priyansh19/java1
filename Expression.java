37816b0c5c5fe7262d02eb58e2c827d33e4dbaf7
priyansh gupta <43648245+priyansh1907@users.noreply.github.com>
2019-03-03 14:05:43 +0530

    Image processing using python

---
+++ Expression.java
@@ -0,0 +1,14 @@
+public class Expression {
+
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		
+		int a;
+		float b;
+		a = Integer.parseInt(args[0]);
+		b = Float.parseFloat(args[1]);
+		int c;
+		c=(int)((int)a+b);
+		System.out.println(c);
+	}
+}
