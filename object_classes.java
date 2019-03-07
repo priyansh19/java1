5b66a1aa97db47c93b074148517e658ceeac924e
priyansh gupta <43648245+priyansh1907@users.noreply.github.com>
2019-03-07 10:21:12 +0530

    This program is of objects and classes of java

---
+++ object_classes.java
@@ -0,0 +1,13 @@
+class Dog {
+void bark(){
+	System.out.println("Bark's");
+}
+}
+public class object_classes extends Dog {
+	public static void main(String[] args) {
+		Dog t = new Dog();
+		t.bark();
+
+	}
+
+}
